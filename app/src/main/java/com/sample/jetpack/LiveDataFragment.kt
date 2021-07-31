8937ef0970d5e5f98a90ee9770f79438dc85fe2e
sunjinbo <sunjinbo@360.cn>
2021-07-31 18:58:26 +0800

    LiveData

--- app/src/main/java/com/sample/jetpack/LiveDataFragment.kt
+++ app/src/main/java/com/sample/jetpack/LiveDataFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class LiveDataFragment : Fragment() {
+
+    private lateinit var viewModel: LiveDataViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_data, container, false)
+        var view = inflater.inflate(R.layout.fragment_view_model, container, false)
+
+        viewModel = ViewModelProvider(this).get(LiveDataViewModel::class.java)
+        var tick = viewModel.getTick()
+        tick.observe(viewLifecycleOwner, {
+            view.post {
+                var tickTextView = view.findViewById<TextView>(R.id.timer)
+                tickTextView.text = it.toString()
+            }
+        })
+        viewModel.startTimer()
+
+        return view
     }
 }
---
+++ app/src/main/java/com/sample/jetpack/LiveDataViewModel.kt
@@ -0,0 +1,31 @@
+package com.sample.jetpack
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LiveDataViewModel : ViewModel(), MyServiceListener.Callback {
+    private var tick: MutableLiveData<Int>? = null
+    private var timer: MyServiceListener? = null
+
+    fun getTick() : LiveData<Int> {
+        if (tick == null) {
+            tick = MutableLiveData()
+        }
+        return tick!!
+    }
+
+    fun startTimer() {
+        timer = MyServiceListener(this)
+        timer?.startTimer()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.stopTimer()
+    }
+
+    override fun onTick(t: Int) {
+        tick?.postValue(t)
+    }
+}
